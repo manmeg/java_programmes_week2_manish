a8965538839a27ead84e8ec9ac63d8cb1897dd40
manmeg <manmeg151@gmail.com>
2023-05-24 19:52:52 +0100

    Homework Submitted

---
+++ src/java_programmes_week2_manish/Programme11_JavaPattern.java
@@ -0,0 +1,29 @@
+package java_programmes_week2_manish;
+
+/** Write a Java program to display the following pattern.
+Sample Pattern :
+J a v v a
+J a a v v a a
+J J aaaaa V V aaaaa
+J J a a V a a
+ */
+
+public class Programme11_JavaPattern
+{
+    public static void main(String[] args)
+    {
+        Programme11_JavaPattern t = new Programme11_JavaPattern();
+        System.out.println("---------------------------------------------");
+        t.javaPattern();
+    }
+
+    // Printing the Java Pattern
+    public void javaPattern()
+    {
+        System.out.println("J  a  v   v a");
+        System.out.println("J a a  v v a a");
+        System.out.println("J J aaaaa  V V aaaaa");
+        System.out.println("J J a   a  V a  a");
+    }
+
+}
